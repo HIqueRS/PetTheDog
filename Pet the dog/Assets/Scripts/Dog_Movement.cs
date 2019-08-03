8f467085dc3088c166949617b18b6d88670679f5
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-08-03 06:44:02 -0300

    Rubing basic

--- Pet the dog/Assets/Scripts/Dog_Movement.cs
+++ Pet the dog/Assets/Scripts/Dog_Movement.cs
@@ -6,18 +6,27 @@ public class Dog_Movement : MonoBehaviour
 {
     
     public GameObject Objective;
-    public float speed = 15000.0f;
+
+    public float speed;
+
+    public Sprite sprite_dog_p;
+    public Sprite sprite_dog_m;
+    public Sprite sprite_dog_g;
+
+    public int pet_var;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<SpriteRenderer>().sprite = sprite_dog_g;
     }
 
     // Update is called once per frame
     void Update()
     {
         float step = speed * Time.deltaTime;
+
+
 
         transform.position = Vector3.MoveTowards(transform.position, Objective.transform.position, step);
 
---
+++ Pet the dog/Assets/Scripts/Rubing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rubing : MonoBehaviour
+{
+
+    public GameObject test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 min_mov = new Vector2(10.0f, 10.0f);
+
+       if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.deltaPosition.x > min_mov.x)
+            {
+                Debug.Log("Ahola");
+            }
+           
+        }
+
+
+
+       
+    }
+
+    
+}
