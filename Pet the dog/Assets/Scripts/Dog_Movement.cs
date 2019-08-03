bf92ac078fa08dd9bc496cd53d95ae1769729e47
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-08-03 16:41:45 -0300

    O ultimo commit antes do sobrecozinhado

--- Pet the dog/Assets/Scripts/Dog_Movement.cs
+++ Pet the dog/Assets/Scripts/Dog_Movement.cs
@@ -10,77 +10,37 @@ public class Dog_Movement : MonoBehaviour
     public GameObject player;
     public GameObject Peting_bar;
 
-    private float speed;
-    public float speed_p;
-    public float speed_m;
-    public float speed_g;
+    public float speed;
 
     public int Size_Tipe;
     private int Max_dog;
-    public Sprite[] sprite_dog_p;
-    public Sprite[] sprite_dog_m;
-    public Sprite[] sprite_dog_g;
+    public Sprite[] sprites_dog;
+    
 
     private float max_pet;
     public int pet_var;
     public Vector2 Min_Mov;
-    public int pet_var_p;
-    public int pet_var_m;
-    public int pet_var_g;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-       
-
-        //Size_Tipe = Random.Range(0, 3);
-
         player = GameObject.Find("Player");
+      
+        Max_dog = sprites_dog.Length;
 
-        switch (Size_Tipe)
-        {
-            case 0:
-                Max_dog = sprite_dog_p.Length;
-
-                GetComponent<SpriteRenderer>().sprite = sprite_dog_p[Random.Range(0,Max_dog)];
-
-                speed = speed_p;
-                pet_var = pet_var_p;
-                break;
-            case 1:
-                Max_dog = sprite_dog_m.Length;
-
-                GetComponent<SpriteRenderer>().sprite = sprite_dog_m[Random.Range(0, Max_dog)];
-
-                speed = speed_m;
-                pet_var = pet_var_m;
-                break;
-            case 2:
-                Max_dog = sprite_dog_g.Length;
-
-                GetComponent<SpriteRenderer>().sprite = sprite_dog_g[Random.Range(0, Max_dog)];
-
-                speed = speed_g;
-                pet_var = pet_var_g;
-                break;
-        }
+        GetComponent<SpriteRenderer>().sprite = sprites_dog[Random.Range(0,Max_dog)];
 
         max_pet = pet_var;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(pet_var * 100 / max_pet);
-
         float step = speed * Time.deltaTime;
-
-        
 
         transform.position = Vector3.MoveTowards(transform.position, Objective.transform.position, step);
 
-        //Vector2 min_mov = new Vector2(10.0f, 10.0f);
 
         if (Input.touchCount > 0)
         {
@@ -89,16 +49,14 @@ public class Dog_Movement : MonoBehaviour
             if (touch.deltaPosition.x > Min_Mov.x || touch.deltaPosition.y > Min_Mov.y)
             {
                 RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
-                // RaycastHit2D can be either true or null, but has an implicit conversion to bool, so we can use it like this
+               
                 if (hitInfo)
                 {
-                    
                     if (gameObject == hitInfo.transform.gameObject)
                     {
                         Debug.Log(hitInfo.transform.gameObject.name);
                         pet_var -= 1;
                     }
-                    // Here you can check hitInfo to see which collider has been hit, and act appropriately.
                 }
             }
         }
@@ -121,7 +79,6 @@ public class Dog_Movement : MonoBehaviour
 
     public float pet_bar()
     {
-
         return (pet_var * 100 / max_pet)/100;
     }
 }
