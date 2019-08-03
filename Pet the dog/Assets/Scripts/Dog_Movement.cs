40a1c372df51bad153a0f26a292c9becf3584d25
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-08-03 01:11:41 -0300

    Simple_Movement

---
+++ Pet the dog/Assets/Scripts/Dog_Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog_Movement : MonoBehaviour
+{
+    
+    public GameObject Objective;
+    public float speed = 15000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, Objective.transform.position, step);
+
+        
+
+    }
+}
