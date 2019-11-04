a724929c224d0d0c6ef0ec43e6e7dde8f829edf3
Henrique lima <riquesouzalima11@gmail.com>
2019-11-04 10:30:15 -0300

    Timerzito

---
+++ Pet the dog/Assets/Scripts/Timerzito.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timerzito : MonoBehaviour
+{
+    public float maxtime;
+    public GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxtime-= Time.deltaTime;
+        text.GetComponent<TMPro.TextMeshProUGUI>().text = maxtime.ToString();
+
+        
+        if(maxtime <=0)
+        {
+            SceneManager.LoadScene("GameOver 1");
+        }
+    }
+}
